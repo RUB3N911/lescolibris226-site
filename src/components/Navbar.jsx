2d8e5c86bc62b39c40231f21f8bd09c6eb164af5
Ruben PELAGE <ecomshop.ruben@gmail.com>
2026-05-15 13:09:10 -0400

    Reduce logo opacity in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -110,7 +110,7 @@ export default function Navbar() {
               src="/images/logo.png"
               alt=""
               aria-hidden="true"
-              className="pointer-events-none absolute left-1/2 top-1/2 z-0 h-[520px] w-[520px] -translate-x-1/2 -translate-y-1/2 object-contain opacity-[0.25]"
+              className="pointer-events-none absolute left-1/2 top-1/2 z-0 h-[520px] w-[520px] -translate-x-1/2 -translate-y-1/2 object-contain opacity-[0.18]"
             />
             {/* LINKS */}
             {links.map((link) => (
