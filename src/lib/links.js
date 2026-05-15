3a2b1b80d1fe97dc3f1da2ec00fcc8d7d2675587
Ruben PELAGE <ecomshop.ruben@gmail.com>
2026-05-14 22:07:17 -0400

    Create links.js with WhatsApp contact links
    Added WhatsApp links for general inquiries, joining, and partnerships.

---
+++ src/lib/links.js
@@ -0,0 +1,8 @@
+export const whatsappGeneral =
+  "https://wa.me/596696298921?text=Bonjour%20Les%20Colibris%20226,%20je%20souhaite%20obtenir%20des%20informations."
+
+export const whatsappJoin =
+  "https://wa.me/596696298921?text=Bonjour%20Les%20Colibris%20226,%20je%20souhaite%20rejoindre%20l’association."
+
+export const whatsappPartner =
+  "https://wa.me/596696298921?text=Bonjour%20Les%20Colibris%20226,%20je%20souhaite%20échanger%20concernant%20un%20partenariat."
